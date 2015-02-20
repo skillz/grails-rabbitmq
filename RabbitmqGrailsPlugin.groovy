c2c12c16727d328ed104df803a392b289b3a5f9f
Joshua Spiewak <jspiewak@skillz.com>
2015-02-20 15:44:23 -0500

    Update version and configure release
    So it can be pushed to Nexus

--- RabbitmqGrailsPlugin.groovy
+++ RabbitmqGrailsPlugin.groovy
@@ -24,7 +24,7 @@ import org.springframework.retry.support.RetryTemplate
 
 class RabbitmqGrailsPlugin {
     // the plugin version
-    def version = "1.0.1"
+    def version = "1.0.1.SKILLZ"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,12 +1,12 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.work.dir = 'target/work'
+grails.project.class.dir = 'target/classes'
+grails.project.test.class.dir = 'target/test-classes'
+grails.project.test.reports.dir = 'target/test-reports'
 
 grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
-    inherits( "global" ) {
+    inherits('global') {
         // uncomment to disable ehcache
         // excludes 'ehcache'
     }
@@ -33,8 +33,12 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build (":release:2.0.4") {
+        build (':release:3.0.1', ':rest-client-builder:1.0.3') {
             export = false
         }
     }
 }
+
+grails.project.repos.releases.url = 'http://nexus.skillz.com/content/repositories/releases/'
+grails.project.repos.default = 'releases'
+grails.release.scm.enabled = false
