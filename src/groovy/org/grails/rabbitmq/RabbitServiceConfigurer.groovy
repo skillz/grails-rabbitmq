b65f77556201ce2a568cf6b3c2f72a0fa048338c
david-thornton717 <41019624+david-thornton717@users.noreply.github.com>
2019-07-15 16:25:18 -0700

    [PS-5543] Increase prefetch count for tournament conclude service and leaderboard service (#1)
    * [PS-5543] Increase prefetch count for tournament conclude service
    
    * Rename constant
    
    * Bump version number
    
    * Add leaderboard service and change type check

--- src/groovy/org/grails/rabbitmq/RabbitServiceConfigurer.groovy
+++ src/groovy/org/grails/rabbitmq/RabbitServiceConfigurer.groovy
@@ -41,6 +41,9 @@ class RabbitServiceConfigurer {
 
     private static final String QUEUE = "rabbitQueue"
     private static final String SUBSCRIBE = "rabbitSubscribe"
+    private static final String TOURNAMENT_CONCLUDE_SERVICE_NAME = 'skillz.TournamentConcludeService'
+    private static final String LEADERBOARD_SERVICE_NAME = 'skillz.stats.LeaderboardService'
+    private static final int PREFETCH_COUNT = 5
     private static final Set ADAPTER_OPTIONS = [
             MESSAGE_CONVERTER_OPTION,
             "defaultListenerMethod",
@@ -216,6 +219,11 @@ class RabbitServiceConfigurer {
             connectionFactory = ref("rabbitMQConnectionFactory")
             concurrentConsumers = serviceConcurrentConsumers
             messageListener = ref("${propertyName}RabbitAdapter")
+
+            if (type == QUEUE && (serviceGrailsClass.clazz.name == TOURNAMENT_CONCLUDE_SERVICE_NAME
+                || serviceGrailsClass.clazz.name == LEADERBOARD_SERVICE_NAME)) {
+                prefetchCount = PREFETCH_COUNT
+            }
             for (entry in containerOptions) {
                 delegate."${entry.key}" = entry.value
             }
